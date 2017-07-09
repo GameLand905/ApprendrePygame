a9ac00d85e2f5c11b03ec1659451f5dfc0c5dbac
GameLand905 <gabriel.barthelemy@gmail.com>
2017-07-09 17:45:25 -0400

    added comments

--- lecon01-rebond.py
+++ lecon01-rebond.py
@@ -1,11 +1,18 @@
+# on dit a Python qu'on utilise Pygame
 import pygame
 
+#le nombre de FPS, c-a-d d'image par seconde
 Nombre_de_FPS = 50
+
+#on compte toujours les millesecondes
 Nombre_de_millisecondes_ecoulee = 0
+
+#les informations sur la fenetre de jeu
 Couleur_du_fond_RVB = (0,0,0)
 Hauteur_de_la_fenetre = 300
 Largeur_de_la_fenetre = 400
 
+#############################################################################
 class Classe_des_PacBoules(pygame.sprite.Sprite):
     def __init__(self, Position_de_depart, Direction_de_depart, Vitesse_de_depart):
         pygame.sprite.Sprite.__init__(self)
@@ -52,17 +59,3 @@ La_boule = Classe_des_PacBoules(Position,Direction,Vitesse)
 ##########################################################################
 # c'est la boucle d'evenement, qui est infinie sauf si ...
 while True:
-    # on attend un peu pour avoir le bon nombre de FPS
-    Nombre_de_millisecondes_ecoulee = Horloge.tick(Nombre_de_FPS)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_game()
-
-    # Redraw the background
-    Fenetre.fill(Couleur_du_fond_RVB)
-
-    # Update and redraw all creeps
-    La_boule.update(Nombre_de_millisecondes_ecoulee)
-
-    pygame.display.flip()
